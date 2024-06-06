9deb28a5d6c408d32536a1ceb69be49bfc571f1c
Matias Rios <matias.rios@docentes.unpaz.edu.ar>
2024-06-06 17:04:53 -0300

    Base de ejercicio TP

---
+++ src/app/services/products.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
+
+export interface Product {
+  _id: string;
+  name: string;
+  description: string;
+  price: number;
+  category: string;
+  image: string;
+  active: boolean;
+}
+
+type ApiResponse = {products:Product[] }
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+
+  httpClient = inject(HttpClient);
+
+  getAll(): Promise<Product[]> {
+    return firstValueFrom(
+      this.httpClient.get<Product[]>('https://6662036463e6a0189fec7284.mockapi.io/products')
+    )
+  }
+
+}
